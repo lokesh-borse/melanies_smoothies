f63f3081ffd6992f495ce3d007d313fa0dce5557
Lokesh borse <149431066+lokesh-borse@users.noreply.github.com>
2026-05-19 12:11:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 # Import python packages
 import streamlit as st
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+import requests
+
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+
 st.text(smoothiefroot_response)
 from snowflake.snowpark.functions import col
 
